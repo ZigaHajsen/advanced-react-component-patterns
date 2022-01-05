4b2ed9250ace5dfcdb07ba93c482d94e9ea47baa
Žiga Hajšen <ziga.hajsen@gmail.com>
2022-01-05 22:34:59 +0100

    cleanup

--- src/patterns/06/useDOMRef.js
+++ src/patterns/06/useDOMRef.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 
 export const useDOMRef = () => {
   const [DOMRef, DOMRefSet] = useState({});
